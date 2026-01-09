2e45686567ab77c8e61c8a2ecc6e560334adb673
Lyman ABID <l.abid@cybertek.fr>
2026-01-09 14:22:42 +0100

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import restart from 'vite-plugin-restart'
 
 export default {
-    root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    root: '', // CORRIGÉ : On laisse vide ou './' car index.html est maintenant à la racine
+    publicDir: 'public/', // CORRIGÉ : Le dossier s'appelle maintenant 'public' (et il est à la racine)
     server:
     {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        host: true, 
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
     },
     build:
     {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        outDir: 'dist', // CORRIGÉ : Plus besoin de '../dist' car on est déjà à la racine
+        emptyOutDir: true, 
+        sourcemap: true 
     },
     plugins:
     [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+        restart({ restart: [ 'public/**', ] }) // CORRIGÉ : On surveille le dossier 'public'
     ],
 }
